133f9523670528000383d0271bdd009a58726f58
karan <patanikaran@gmail.com>
2024-04-02 15:54:06 +0530

    add supply chain db

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
@@ -11,3 +12,14 @@ class CustomUser(AbstractUser):
         ('receptionist', 'Receptionist'),
     )
     role = models.CharField(max_length=50, choices=ROLE_CHOICES)
+    
+    def save(self, *args, **kwargs):
+        if not self.pk or self.password:
+            # If the user is being created (not updated),
+            # set_password is used to hash the password.
+            self.set_password(self.password)
+        
+        super().save(*args, **kwargs)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
